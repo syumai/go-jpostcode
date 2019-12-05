29469938bda7427fad1ed75df1a52c60ccd6b3e5
syumai <syumai@gmail.com>
2019-12-06 01:08:48 +0900

    Fix conversion bug

--- address.go
+++ address.go
@@ -3,17 +3,17 @@ package jpostcode
 import "encoding/json"
 
 type Address struct {
-	PostCode       string `json:"postCode"`
-	Prefecture     string `json:"prefecture"`
-	PrefectureKana string `json:"prefectureKana"`
-	PrefectureCode string `json:"prefectureCode"`
-	City           string `json:"city"`
-	CityKana       string `json:"cityKana"`
-	Town           string `json:"town"`
-	TownKana       string `json:"townKana"`
-	Street         string `json:"street"`
-	OfficeName     string `json:"officeName"`
-	OfficeNameKana string `json:"officeNameKana"`
+	PostCode       string `json:"postCode" mapstructure:"postcode"`
+	Prefecture     string `json:"prefecture" mapstructure:"prefecture"`
+	PrefectureKana string `json:"prefectureKana" mapstructure:"prefecture_kana"`
+	PrefectureCode int    `json:"prefectureCode" mapstructure:"prefecture_code"`
+	City           string `json:"city" mapstructure:"city"`
+	CityKana       string `json:"cityKana" mapstructure:"city_kana"`
+	Town           string `json:"town" mapstructure:"town"`
+	TownKana       string `json:"townKana" mapstructure:"town_kana"`
+	Street         string `json:"street" mapstructure:"street"`
+	OfficeName     string `json:"officeName" mapstructure:"office_name"`
+	OfficeNameKana string `json:"officeNameKana" mapstructure:"office_name_kana"`
 }
 
 func (addr *Address) ToJSON() (string, error) {
