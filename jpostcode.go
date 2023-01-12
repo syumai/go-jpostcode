543b0823b0d7696372865d5883017641b28be00e
najeira <najeira@gmail.com>
2023-01-12 14:26:06 +0900

    delay adapter initialization until first use

--- jpostcode.go
+++ jpostcode.go
@@ -1,5 +1,14 @@
 package jpostcode
 
+import (
+	"sync"
+)
+
+var (
+	adapter     Adapter
+	adapterOnce sync.Once
+)
+
 func Find(postCode string) (*Address, error) {
 	addresses, err := Search(postCode)
 	if err != nil {
@@ -12,5 +21,15 @@ func Find(postCode string) (*Address, error) {
 }
 
 func Search(postCode string) ([]*Address, error) {
+	adapterOnce.Do(func() {
+		// set default adapter
+		var err error
+		adapter, err = newBadgerAdapter()
+		if err != nil {
+			panic(err)
+		}
+		// closing DB is not needed because badger adapter is using in-memory DB
+		// adapter.db.Close()
+	})
 	return adapter.SearchAddressesFromPostCode(postCode)
 }
